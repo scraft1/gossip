6a1605cc0753b7e5081069a2e80b7328a5b96ad4
scraft1 <spencer.craft.90@gmail.com>
2015-04-07 11:46:46 -0600

    fixed welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
 	<body>
 		<div class="container">
 			<div class="content">
-				<div class="title">Lab 5: Gossip Protocol</div>
+				<div class="title">Welcome!</div>
 			</div>
 		</div>
 	</body>
